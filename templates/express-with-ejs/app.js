4ba28e29ee8e944f19c41d7baf9090f787591cd8
Emmanuel Gyang <emmanuel.gyang@hubtel.com>
2024-03-17 00:52:20 +0000

    finished with basic node express ejs file

--- templates/express-with-ejs/app.js
+++ templates/express-with-ejs/app.js
@@ -1,13 +1,16 @@
 // templates/app.js
 
+// eslint-disable-next-line import/no-unresolved
 import express from 'express';
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Hello, <%= projectName %>!');
+  res.render('index', { projectName: '<%= projectName %>' });
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on http://localhost:3000/ 😎...');
 });
