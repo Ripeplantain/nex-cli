0794ccc1e915847b1016eddc490cf2cee352587b
Emmanuel Gyang <irvingmanny@gmail.com>
2024-04-14 00:40:22 +0000

    added route template and index route.js

---
+++ constants/routeTemplate.js
@@ -0,0 +1,8 @@
+const routeTemplate = `import express from 'express';
+
+const router = express.Router();
+
+export default router;
+`;
+
+export default routeTemplate;
--- utils/route.js
+++ utils/route.js
@@ -1,15 +1,25 @@
-import { exec } from 'child_process';
+import fs from 'fs';
+import routeTemplate from '../constants/routeTemplate.js';
 
 export default function addRoute(name) {
   const routeName = name.toLowerCase();
+  const routesDirectory = 'src/routes';
+  const indexFilePath = 'src/routes/index.js';
+
   try {
-    exec(
-      `cd src && mkdir routes && cd routes && touch ${routeName}.routes.js`,
-      error => {
-        if (error) {
-          exec(`mkdir routes && cd routes && touch ${routeName}.routes.js`);
-        }
-      }
+    if (!fs.existsSync(routesDirectory)) {
+      fs.mkdirSync(routesDirectory, { recursive: true });
+    }
+
+    if (fs.existsSync(indexFilePath)) {
+      fs.appendFileSync(indexFilePath, routeTemplate);
+    } else {
+      fs.writeFileSync(indexFilePath, routeTemplate);
+    }
+
+    fs.writeFileSync(
+      `${routesDirectory}/${routeName}.routes.js`,
+      routeTemplate
     );
   } catch (error) {
     console.error('Error creating route:', error.message);
