5cebbc567e85ff8cef0ae130727e7a9ec2274eac
Emmanuel Gyang <irvingmanny@gmail.com>
2024-05-05 00:35:03 +0000

    service implementation changed

---
+++ constants/serviceTemplate.js
@@ -0,0 +1,3 @@
+export default function serviceTemplate() {
+  return `//Add Service Template`;
+}
--- utils/service.js
+++ utils/service.js
@@ -1,18 +1,20 @@
-import { exec } from 'child_process';
+import fs from 'fs';
+import serviceTemplate from '../constants/serviceTemplate.js';
+import { doesProjectExist } from './file.js';
 
 export default function addService(name) {
   const serviceName = name.charAt(0).toLowerCase() + name.slice(1);
+  const serviceDirectory = 'src/controllers';
+  const isTypescript = doesProjectExist('tsconfig.json');
 
   try {
-    exec(
-      `cd src && mkdir services && cd services && touch ${serviceName}.service.js`,
-      error => {
-        if (error) {
-          exec(
-            `mkdir services && cd services && touch ${serviceName}.service.js`
-          );
-        }
-      }
+    if (!fs.existsSync(serviceDirectory)) {
+      fs.mkdirSync(serviceDirectory, { recursive: true });
+    }
+
+    fs.writeFileSync(
+      `${serviceDirectory}/${serviceName}.service.${isTypescript ? 'ts' : 'ts'}`,
+      serviceTemplate
     );
   } catch (error) {
     console.error('Error creating service:', error.message);
