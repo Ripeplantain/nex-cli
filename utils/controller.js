737c4a94bcc8876d4330be53cd71f7b983901fb3
Emmanuel Gyang <irvingmanny@gmail.com>
2024-05-05 00:01:53 +0000

    controller generation changed

--- utils/controller.js
+++ utils/controller.js
@@ -1,22 +1,25 @@
-import { exec } from 'child_process';
 import fs from 'fs';
 import {
   javascriptControllerTemplate,
   typescriptControllerTemplate,
-} from '../constants/controllerTemplate';
+} from '../constants/controllerTemplate.js';
+import { doesProjectExist } from './file.js';
 
 export default function addController(name) {
   const controllerName = name.charAt(0).toUpperCase() + name.slice(1);
+  const controllersDirectory = 'src/controllers';
+  const isTypescript = doesProjectExist('tsconfig.json');
+
   try {
-    exec(
-      `cd src && mkdir controllers && cd controllers && touch ${controllerName}Controller.js`,
-      error => {
-        if (error) {
-          exec(
-            `mkdir controllers && cd controllers && touch ${controllerName}Controller.js`
-          );
-        }
-      }
+    if (!fs.existsSync(controllersDirectory)) {
+      fs.mkdirSync(controllersDirectory, { recursive: true });
+    }
+
+    fs.writeFileSync(
+      `${controllersDirectory}/${controllerName}.${isTypescript ? 'ts' : 'js'}`,
+      isTypescript
+        ? typescriptControllerTemplate(controllerName)
+        : javascriptControllerTemplate(controllerName)
     );
   } catch (error) {
     console.error('Error creating controller:', error.message);
--- utils/file.js
+++ utils/file.js
@@ -8,3 +8,6 @@ export default function getDirectoriesInTemplates() {
   return fs.readdirSync(templateDir);
 }
 
+export function doesProjectExist(filePath) {
+  return fs.existsSync(filePath, { recursive: true });
+}
